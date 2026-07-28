2d3f3847e7f21e8cb80852c209bcab709327273a
Vũ Minh Thành <52306715+thanhthbm@users.noreply.github.com>
2026-07-28 16:12:03 +0700

    Add: pascals-triangle-ii - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+
+        res = [1]
+        previous_row: list[int] = self.getRow(rowIndex - 1)
+
+        for i in range(1, rowIndex):
+            res.append(previous_row[i] + previous_row[i - 1])
+
+        res.append(1)
+
+        return res
