b489f0c118ab8436d4f1329f96cb4db8eb13da5d
Vũ Minh Thành <52306715+thanhthbm@users.noreply.github.com>
2026-08-25 09:30:39 +0700

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        people = list(zip(names, heights))
+        people.sort(key=lambda x: x[1], reverse=True)
+
+        return [name for name, height in people]
